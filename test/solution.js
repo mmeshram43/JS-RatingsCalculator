c383387a6ac82c0cd5ca386143991657e0cabb48
Mayur Meshram <mmeshram43@gmail.com>
2021-11-28 15:56:15 +0530

    First Commit

--- test/solution.js
+++ test/solution.js
@@ -2,8 +2,12 @@ const collect_ratings = () => {
   const ratings = { 'count': 0, 'sum': 0, 'average': 0 };
 
   let rating = 0;
+
   const elements = document.querySelectorAll('.rating');
+  console.log(elements);
+
   elements.forEach(element => {
+
     rating = parseInt(element.id.replace('star', ''));
     ratings.count += parseInt(element.value);
     ratings.sum += parseInt(element.value) * rating;
